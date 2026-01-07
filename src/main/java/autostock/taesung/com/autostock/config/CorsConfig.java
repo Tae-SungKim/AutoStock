6198b229ec058fcf76a3444b3a30b4f3c83de55d
Tae-SungKim <gene779@gmail.com>
2026-01-08 08:36:59 +0900

    Update CorsConfig.java
    모든 origin허용

--- src/main/java/autostock/taesung/com/autostock/config/CorsConfig.java
+++ src/main/java/autostock/taesung/com/autostock/config/CorsConfig.java
@@ -13,7 +13,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOriginPatterns("*")  // 모든 Origin 허용
                         .allowedMethods("GET","POST","PUT","DELETE","OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
